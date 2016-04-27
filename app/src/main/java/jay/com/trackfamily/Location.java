485c71b57bd2715fe137f86dd8c11dbcb6e12115
Java Programs <javaprograms4u@gmail.com>
2016-04-28 00:57:39 +0530

    Added FusedLocationProvider

--- app/src/main/java/jay/com/trackfamily/Location.java
+++ app/src/main/java/jay/com/trackfamily/Location.java
@@ -1,45 +1,54 @@
 package jay.com.trackfamily;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 /**
  * Created by Jay on 28-04-2016.
  */
-public class Location extends Service implements LocationListener,GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+public class Location extends Service implements LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final long INTERVAL = 1000 * 10;
     private static final long FASTEST_INTERVAL = 1000 * 5;
     LocationRequest mLocationRequest;
     GoogleApiClient mGoogleApiClient;
-    Location mCurrentLocation;
+    android.location.Location mCurrentLocation;
     String mLastUpdateTime;
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        Toast.makeText(getApplicationContext(),"onBind",Toast.LENGTH_SHORT).show();
         return null;
     }
 
     @Override
-    public void onStart(Intent intent, int startId) {
-        super.onStart(intent,startId);
+    public void onCreate() {
+        super.onCreate();
         if (isGooglePlayServicesAvailable()) {
-            if (mGoogleApiClient != null) {
+            if (mGoogleApiClient == null) {
                 createLocationRequest();
-                super.onStart(intent, startId);
                 mGoogleApiClient = new GoogleApiClient.Builder(this)
                         .addApi(LocationServices.API)
                         .addConnectionCallbacks(this)
@@ -47,52 +56,70 @@ public class Location extends Service implements LocationListener,GoogleApiClien
                         .build();
             }
         }
+        Toast.makeText(getApplicationContext(),"onCreate",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+        if (mGoogleApiClient != null && !mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.connect();
+        }
+        Toast.makeText(getApplicationContext(),"onStart",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (isGooglePlayServicesAvailable()) {
-            if (mGoogleApiClient != null) {
-                createLocationRequest();
-                super.onStart(intent, startId);
-                mGoogleApiClient = new GoogleApiClient.Builder(this)
-                        .addApi(LocationServices.API)
-                        .addConnectionCallbacks(this)
-                        .addOnConnectionFailedListener(this)
-                        .build();
-            }
+        if (mGoogleApiClient != null && !mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.connect();
         }
+        Toast.makeText(getApplicationContext(),"onStartCommand",Toast.LENGTH_SHORT).show();
         return super.onStartCommand(intent, flags, startId);
     }
 
-    private void createLocationRequest(){
+    private void createLocationRequest() {
         mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(INTERVAL);
         mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        Toast.makeText(getApplicationContext(),"createLocationRequest",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        Toast.makeText(getApplicationContext(),"onConnected",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        Toast.makeText(getApplicationContext(),"onConnectionSuspended",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onLocationChanged(android.location.Location location) {
-
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        Toast.makeText(getApplicationContext(),"Lattitude : "+location.getLatitude()+"\nLongitude : "+location.getLongitude(),Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        Toast.makeText(getApplicationContext(),"onConnectionFailed",Toast.LENGTH_SHORT).show();
     }
 
     private boolean isGooglePlayServicesAvailable() {
+        Toast.makeText(getApplicationContext(),"isGooglePlayServicesAvailable",Toast.LENGTH_SHORT).show();
         int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
         if (ConnectionResult.SUCCESS == status) {
             return true;
--- app/src/main/java/jay/com/trackfamily/MainActivity.java
+++ app/src/main/java/jay/com/trackfamily/MainActivity.java
@@ -1,7 +1,9 @@
 package jay.com.trackfamily;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
@@ -12,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, Location.class);
+        Toast.makeText(getApplicationContext(),"intent",Toast.LENGTH_SHORT).show();
+        startService(intent);
     }
 }
